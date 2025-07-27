abfea6b29a97b6ba60ef82f772f5ddc9c13ff139
Gaku1031 <gaku.1031.basket@gmail.com>
2025-07-28 00:14:55 +0900

    ffmpegの引数を修正し、フレームレートの指定方法を改善しました。これにより、動画処理の精度が向上することが期待されます。

--- CppBloodPressureDLL/test/CSharpTest.cs
+++ CppBloodPressureDLL/test/CSharpTest.cs
@@ -289,7 +289,7 @@ namespace BloodPressureDllTest
                 
                 var ffmpegProc = new System.Diagnostics.Process();
                 ffmpegProc.StartInfo.FileName = ffmpegExe;
-                ffmpegProc.StartInfo.Arguments = $"-y -i \"{sampleVideo}\" -ss 20 -t 30 -vf \"scale=640:480,fps=15\" -q:v 2 \"{framePattern}\"";
+                ffmpegProc.StartInfo.Arguments = $"-y -i \"{sampleVideo}\" -ss 20 -t 30 -vf \"scale=640:480,fps=fps=15\" -q:v 2 \"{framePattern}\"";
                 ffmpegProc.StartInfo.UseShellExecute = false;
                 ffmpegProc.StartInfo.RedirectStandardOutput = true;
                 ffmpegProc.StartInfo.RedirectStandardError = true;
